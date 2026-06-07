79103ed24e14b3579f64eef2017ec048e9037ad8
Nupur Srivastava <70944857+Nupur-Srivastava@users.noreply.github.com>
2026-06-07 18:49:21 +0530

    Update AuthService.java

--- demo/src/main/java/com/example/aurum/demo/service/AuthService.java
+++ demo/src/main/java/com/example/aurum/demo/service/AuthService.java
@@ -58,6 +58,6 @@ public class AuthService {
         }
 
         String token = jwtService.generateToken(user.getEmail());
-        return new AuthResponse(user.getId(), "Login Successful",user.getEmail(),user.getPassword(),token);
+        return new AuthResponse(user.getId(), "Login Successful",user.getEmail(),null,token);
     }
 }
