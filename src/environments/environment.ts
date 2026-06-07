0eba196096bd985dabf92e7b66aaa1600ba1cb41
Nupur Srivastava <70944857+Nupur-Srivastava@users.noreply.github.com>
2026-06-07 15:56:10 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:8080/api'  // Your backend partner's local server
+  production: true,
+  apiUrl: 'https://aurum-development-production.up.railway.app/api'
 };
