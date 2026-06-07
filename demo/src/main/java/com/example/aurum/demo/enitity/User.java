5b6f1b024be9f83228cda78b64405c7958764fea
Nupur Srivastava <70944857+Nupur-Srivastava@users.noreply.github.com>
2026-06-07 13:49:34 +0530

    Update User.java

--- demo/src/main/java/com/example/aurum/demo/enitity/User.java
+++ demo/src/main/java/com/example/aurum/demo/enitity/User.java
@@ -1,26 +1,36 @@
 package com.example.aurum.demo.enitity;
 
+import com.example.aurum.demo.enumns.ProjectSizeUnit;
 import jakarta.persistence.*;
-import org.springframework.data.annotation.Id;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.Date;
 
 @Entity
+@Getter
+@Setter
 @Table(name = "users")
 public class User {
 
-    @Id
+    @Id  // ✅ THIS IS THE FIX
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String fullName;
 
-    @Column(unique = true)
+    private String fullName;
     private String email;
-    @Column(unique = true)
     private String phone;
     private String password;
     private String category;
     private String provider;
     private Boolean verified;
+    private Integer yearsOfExperience;
+    private String subCategory;
+    private Integer projectSize;
     private Date createdAt;
+
+    @Enumerated(EnumType.STRING)
+    private ProjectSizeUnit projectSizeUnit;
+
+    public User() {}
 }
