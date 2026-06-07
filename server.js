dd7dc7be478de08956249d8e0377de974671c222
Nupur Srivastava <70944857+Nupur-Srivastava@users.noreply.github.com>
2026-06-07 20:51:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist/aec-platform/browser')));
+const distPath = path.join(__dirname, 'dist/aec-platform');
+
+app.use(express.static(distPath));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/aec-platform/browser/index.html'));
+  res.sendFile(path.join(distPath, 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
