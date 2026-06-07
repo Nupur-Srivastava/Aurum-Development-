38715b339816f1f2fda154c4df619c513a8046c6
Nupur Srivastava <70944857+Nupur-Srivastava@users.noreply.github.com>
2026-06-07 19:10:28 +0530

    Update AuthService.java

--- demo/src/main/java/com/example/aurum/demo/service/AuthService.java
+++ demo/src/main/java/com/example/aurum/demo/service/AuthService.java
@@ -46,7 +46,7 @@ public class AuthService {
         user.setYearsOfExperience(request.getYearsOfExperience());
 
         user = userRepository.save(user);
-        return new AuthResponse(user.getId(), "Signup Successful",user.getEmail(),token);
+        return new AuthResponse(user.getId(), "Signup Successful",user.getEmail(),user.getPassword());
     }
     public AuthResponse login(AuthRequest request) {
 
